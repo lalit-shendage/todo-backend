d7123d7d3671d9055aaf81ae97175cc5405a81a9
lalit-shendage <shendage.lalit@gmail.com>
2023-10-31 22:35:07 +0530

    fixing mongoose issue for deployment

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); 
+user=process.env.USER;
 
-
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(`mongodb+srv://${user}@cluster0.dwnwv8t.mongodb.net/todo-mern`);
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to the database');
